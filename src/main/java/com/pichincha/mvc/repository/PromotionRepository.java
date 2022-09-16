0bd019204126b3367d9fcf61ab489fd4e2f43a2c
Bladimir Minga <bsminga@pichincha.com>
2022-09-16 11:22:22 -0500

    The repository layer is added

--- src/main/java/com/pichincha/mvc/repository/PromotionRepository.java
+++ src/main/java/com/pichincha/mvc/repository/PromotionRepository.java
@@ -1,10 +1,9 @@
 package com.pichincha.mvc.repository;
 
-import com.pichincha.mvc.domain.ProductEntity;
+import com.pichincha.mvc.domain.PromotionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TemplateRepository extends JpaRepository<ProductEntity, Long> {
-
+public interface PromotionRepository extends JpaRepository<PromotionEntity, Long> {
 }
